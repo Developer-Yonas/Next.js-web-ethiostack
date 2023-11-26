9f5ab8b13759c0363baccb47d3b6518d6303bd77
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2023-11-26 20:26:43 +0300

    first commit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -29,8 +29,7 @@ const Feed = () => {
   const fetchPosts = async () => {
     // const response = await fetch(`/api/prompt?timestamp=${new Date().getTime()}`);
 
-    const baseUrl = process.env.PUBLIC_URL || ''; // Use PUBLIC_URL if available
-    const response = await fetch(`${baseUrl}/api/prompt`);
+    const response = await fetch(`/api/prompt`);
 
     const data = await response.json();
 
