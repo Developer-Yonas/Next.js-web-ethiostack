b910e2a62ac03ececec1dca73e71ad4e53ea69a1
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2023-11-26 10:41:10 +0300

    first commit

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -34,7 +34,7 @@ const UpdatePrompt = () => {
     if (!promptId) return alert("Missing PromptId!");
 
     try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
+      const response = await fetch(`https://ethiostack.vercel.app/api/prompt/${promptId}`, {
         method: "PATCH",
         body: JSON.stringify({
           prompt: post.prompt,
