a210bf245711e18c48899099035a049231637dd0
Yonaswg <110293537+Yonaswg@users.noreply.github.com>
2023-12-04 23:33:40 +0300

    fourth commit

--- app/api/prompt/server.js
+++ app/api/prompt/server.js
@@ -10,10 +10,33 @@ const PORT = 3000;
 mongoose.connect('mongodb://ethiostack.vercel.app/share_prompt', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Create a WebSocket server
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({
+  noServer: true,
+  verifyClient: (info, callback) => {
+    // Add logic to verify the origin, e.g., check against a whitelist
+    const isValid = /* Your logic to validate the origin */;
+    callback(isValid);
+  },
+});
 
-wss.on('connection', function connection(ws) {
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
+});
+
+wss.on('connection', function connection(ws, req) {
   console.log('A new client connected');
+
+  // Add logic to handle CORS, e.g., check req.headers.origin
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
 });
 
 // API endpoint to fetch prompts
@@ -26,10 +49,4 @@ app.get('/api/prompt', async (req, res) => {
     console.error('Failed to fetch prompts:', error); // Log the error
     res.status(500).json({ error: 'Failed to fetch prompts' });
   }
-});
-
-
-// Start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
